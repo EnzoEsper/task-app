2a9a302cccc78060437f0dcd0ac78327334992a1
enzoesper <enzoesper@hotmail.com>
2019-10-15 07:53:14 -0300

    creating and persisting a mongoose model part2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,24 +5,47 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useCreateIndex: true
 });
 
-const User = mongoose.model("User", {
-  name: {
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String
+//   },
+//   age: {
+//     type: Number
+//   }
+// });
+
+// const me = new User({
+//   name: "Enzo",
+//   age: "dada"
+// });
+
+// me.save()
+//   .then(me => {
+//     console.log(me);
+//   })
+//   .catch(error => {
+//     console.log("Error!", error);
+//   });
+
+const Task = mongoose.model("Task", {
+  description: {
     type: String
   },
-  age: {
-    type: Number
+  completed: {
+    type: Boolean
   }
 });
 
-const me = new User({
-  name: "Enzo",
-  age: "dada"
+const task = new Task({
+  description: "Learn the mongoose library",
+  completed: false
 });
 
-me.save()
-  .then(me => {
-    console.log(me);
+task
+  .save()
+  .then(task => {
+    console.log(task);
   })
   .catch(error => {
-    console.log("Error!", error);
+    console.log(error);
   });
