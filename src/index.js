e1451bf3a06612ff7d5942990900fa125e21aeaf
enzoesper <enzoesper@hotmail.com>
2019-10-22 21:36:18 -0300

    Resource reading endpoints part 2 (fetching all tasks and an individual task by its id)

--- src/index.js
+++ src/index.js
@@ -71,6 +71,34 @@ app.post("/tasks", (req, res) => {
     });
 });
 
+//fetching all the tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then(tasks => {
+      res.send(tasks);
+    })
+    .catch(e => {
+      res.status(500).send();
+    });
+});
+
+// fetching an individual task
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch(() => {
+      res.status(500).send();
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port", port);
 });
