bfe17450944c9329f477ee244e746bf3cc0b8372
enzoesper <enzoesper@hotmail.com>
2019-10-25 00:01:31 -0300

    integrating AsyncAwait to the task-manager app

--- src/index.js
+++ src/index.js
@@ -12,91 +12,92 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // creating an user
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+
+  // user
+  //   .save()
+  //   .then(() => {
+  //     res.status(201).send(user);
+  //   })
+  //   .catch(e => {
+  //     res.status(400).send(e);
+  //   });
 });
 
 // fetching all the users
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
   // this is going to fetch all the users stored in the db
-  User.find({})
-    .then(users => {
-      res.send(users);
-    })
-    .catch(e => {
-      // Internal server error
-      res.status(500).send();
-    });
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // fetching a individual user by id
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
+  try {
+    // Mongo db not consider a faillure if dont get any result back when were
+    // looking for something -> that is consider a success (even is no matches, returns nothing)
+    const user = await User.findById(_id);
 
-  User.findById(_id)
-    .then(user => {
-      // Mongo db not consider a faillure if dont get any result back when were
-      // looking for something -> that is consider a success (even is no matches, returns nothing)
-      if (!user) {
-        return res.status(404).send();
-      }
+    if (!user) {
+      return res.status(404).send();
+    }
 
-      res.send(user);
-    })
-    .catch(e => {
-      res.status(500).send(e);
-    });
+    res.send(user);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // creating a task
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
 
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 //fetching all the tasks
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then(tasks => {
-      res.send(tasks);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // fetching an individual task
-app.get("/tasks/:id", (req, res) => {
+app.get("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
 
-  Task.findById(_id)
-    .then(task => {
-      if (!task) {
-        return res.status(404).send();
-      }
+  try {
+    const task = await Task.findById(_id);
 
-      res.send(task);
-    })
-    .catch(() => {
-      res.status(500).send();
-    });
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => {
