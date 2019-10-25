670b992c1d179525074d62f9207ef7e2df47e48a
enzoesper <enzoesper@hotmail.com>
2019-10-24 23:22:01 -0300

    Using AsyncAwait

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,13 +3,27 @@ const User = require("../src/models/user");
 
 // Use promise chaingin to do one thing after another
 // We update the age of a certain user and then we fetch all users with the same age
-User.findByIdAndUpdate("5da6a41f8924b01e4896e45e", { age: 17 })
-  .then(user => {
-    console.log(user);
-    return User.countDocuments({ age: 17 });
-  })
-  .then(result => {
-    console.log(result);
+// User.findByIdAndUpdate("5da6a41f8924b01e4896e45e", { age: 17 })
+//   .then(user => {
+//     console.log(user);
+//     return User.countDocuments({ age: 17 });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age: age });
+  const count = await User.countDocuments({ age: age });
+  return count;
+};
+
+updateAgeAndCount("5da6a41f8924b01e4896e45e", 33)
+  .then(count => {
+    console.log(count);
   })
   .catch(e => {
     console.log(e);
