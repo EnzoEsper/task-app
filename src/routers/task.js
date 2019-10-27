56b4033c4e575f7c8ca914eee4c411ed280bd0a9
enzoesper <enzoesper@hotmail.com>
2019-10-27 19:54:17 -0300

    athenticating task endpoints: fetching an individual task from an user and all the task for a certain user

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,22 +19,25 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
-//fetching all the tasks
-router.get("/tasks", async (req, res) => {
+//fetching all the tasks for a certain user
+router.get("/tasks", auth, async (req, res) => {
   try {
-    const tasks = await Task.find({});
-    res.send(tasks);
+    // const tasks = await Task.find({ owner: req.user._id }); this is another approach to the code below
+    await req.user.populate("tasks").execPopulate();
+
+    res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send();
   }
 });
 
 // fetching an individual task
-router.get("/tasks/:id", async (req, res) => {
+router.get("/tasks/:id", auth, async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const task = await Task.findById(_id);
+    // const task = await Task.findById(_id);
+    const task = await Task.findOne({ _id, owner: req.user._id });
 
     if (!task) {
       return res.status(404).send();
