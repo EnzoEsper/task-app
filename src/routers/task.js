fde6ed2fca170c0b96fb256babd1fa6cecde8641
enzoesper <enzoesper@hotmail.com>
2019-10-27 20:04:40 -0300

    authenticating task endpoint: deleting a task of a certain user

--- src/routers/task.js
+++ src/routers/task.js
@@ -87,12 +87,12 @@ router.patch("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-// deleting a task
-router.delete("/tasks/:id", async (req, res) => {
+// deleting a task of a certain user
+router.delete("/tasks/:id", auth, async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const task = await Task.findByIdAndDelete(_id);
+    const task = await Task.findOneAndDelete({ _id, owner: req.user._id });
 
     if (!task) {
       res.status(404).send();
