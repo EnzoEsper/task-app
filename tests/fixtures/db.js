bb0971d384e76ec283bbf1b889195d21e2bd4b18
Enzo Esper <enzoesper@hotmail.com>
2019-11-24 19:35:26 -0300

    Testing with task data

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -2,6 +2,7 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const User = require("../../src/models/user");
+const Task = require("../../src/models/task");
 
 const userOneId = new mongoose.Types.ObjectId();
 
@@ -17,15 +18,60 @@ const userOne = {
   ]
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: "Mike2",
+  email: "mike2@gmail.com",
+  password: "mike212345!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET)
+    }
+  ]
+};
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "First task from suite",
+  owner: userOneId
+};
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Second task from suite",
+  completed: true,
+  owner: userOneId
+};
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: "Second task from suite",
+  completed: true,
+  owner: userTwo._id
+};
+
 const setupDatabase = async () => {
   // Deleting all users from the db everytime a test is run, to start from a empty db to all tests
   await User.deleteMany();
+  // Deleting all tasks from the db
+  await Task.deleteMany();
   // Creating an example user for testing the login endpoint case
   await new User(userOne).save();
+  await new User(userTwo).save();
+  // Creating the three tasks
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
 };
 
 module.exports = {
   userOneId,
   userOne,
-  setupDatabase
+  setupDatabase,
+  userTwoId,
+  userTwo,
+  taskOne,
+  taskTwo,
+  taskThree
 };
